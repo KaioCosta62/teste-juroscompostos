fde60eee92e4e431915aea411be29d01a5cd2ff3
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-01 11:11:40 -0300

    adicionando funcao que permite apenas numeros nos campos de mensalidade e taxa de juros

--- js/script.js
+++ js/script.js
@@ -23,21 +23,56 @@ function simulateInvesitments(){
   const form = document.querySelector('#simulator')
   const firstPage = document.querySelector('.first-page')
   const secondPage = document.querySelector('.second-page')
-  const btnSimulateAgain = document.querySelector('.second-page button')
 
   form.addEventListener('submit', function(e){
     e.preventDefault()
+    let verifyError = false
 
-    const name = document.forms['simulator']['name'].value
+    const inputName = document.forms['simulator']['name']
+    const regexName = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/
+    const verifyName = regexName.test(inputName.value)
 
-    const mensality = document.forms['simulator']['mensality'].value
-    const mensalityNumber = Number(mensality.replace(',','.').replace('R$',''))
+    if(!verifyName){
+      verifyError = true
+      inputName.classList.add('error-input')
+      let span = inputName.nextElementSibling
+      span.innerHTML = 'Por favor, insira seu nome'
+    }else{
+      inputName.classList.remove('error-input')
+      let span = inputName.nextElementSibling
+      span.innerHTML = ''
+    }
 
-    const interestRate = document.forms['simulator']['rate'].value
-    const interestRateNumber = Number(interestRate.replace('%', '').replace(',','.'))
+    const inputMensality = document.forms['simulator']['mensality']
+    const mensalityNumber = Number(inputMensality.value.replace(',','.').replace('R$',''))
 
-    const timeInvestiment = document.forms['simulator']['timeYears'].value
-    const timeInvestimentNumber = Number(timeInvestiment)
+    if(!inputMensality.value){
+      verifyError = true
+      inputMensality.classList.add('error-input')
+      let span = inputMensality.nextElementSibling
+      span.innerHTML = 'Por favor, insira um valor de mensalidade'
+    }else{
+      inputMensality.classList.remove('error-input')
+      let span = inputName.nextElementSibling
+      span.innerHTML = ''
+    }
+
+    const inputInterestRate = document.forms['simulator']['rate']
+    const interestRateNumber = Number(inputInterestRate.value.replace('%', '').replace(',','.'))
+
+    if(!inputInterestRate.value){
+      verifyError = true
+      inputInterestRate.classList.add('error-input')
+      let span = inputInterestRate.nextElementSibling
+      span.innerHTML = 'Por favor, insira uma taxa de juros'
+    }else{
+      inputInterestRate.classList.remove('error-input')
+      let span = inputInterestRate.nextElementSibling
+      span.innerHTML = ''
+    }
+
+    const inputTimeInvestiment = document.forms['simulator']['timeYears']
+    const timeInvestimentNumber = Number(inputTimeInvestiment.value)
 
     const configApi = {
       header: {
@@ -52,29 +87,44 @@ function simulateInvesitments(){
     }
 
     function apiData(data){
-      let returnInvestiment = data.result
-      console.log(returnInvestiment)
+      if(!verifyError){
+        let returnInvestiment = Number(data.result)
+        let estructureHTML = `
+        <div class="container">
+          <h3>Money Investiments</h3>
+          <p>Olá ${inputName.value}, investindo R$ ${mensalityNumber} todo mês, você terá R$ ${returnInvestiment.toFixed(2)} em ${timeInvestimentNumber} anos</p>
+          <button>Simular novamente</button>
+        </div>
+        `
+        secondPage.innerHTML = estructureHTML
+        firstPage.style.display = 'none'
+        secondPage.style.display = 'block'
+      }    
     }
 
     fetch('http://api.mathjs.org/v4/', configApi).then(transformedJson).then(apiData)
-    firstPage.style.display = 'none'
-    secondPage.style.display = 'flex'
   })
 
- 
- 
 }
 
 function simulateAgain(){
   const firstPage = document.querySelector('.first-page')
   const secondPage = document.querySelector('.second-page')
   const btnSimulateAgain = document.querySelector('.second-page button')
-
-  btnSimulateAgain.addEventListener('click', function(){
-    firstPage.style.display = 'flex'
-    secondPage.style.display = 'none'
-  })
 }
+
+function onlyNumber(evt) {
+  const theEvent = evt || window.event;
+  let key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  const regex = /^[0-9.]+$/;
+  if( !regex.test(key) ) {
+     theEvent.returnValue = false;
+     if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
 yearsInvestiments()
 simulateInvesitments()
 simulateAgain()
+
